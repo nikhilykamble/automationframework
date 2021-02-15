a40dee7e2470402f80947a92e20df5aa6efdb24c
Nikhil <nikhilykamble@gmail.com>
2021-02-15 22:27:34 +0530

    second commit

--- src/One/FirstPage.java
+++ src/One/FirstPage.java
@@ -5,7 +5,11 @@ public class FirstPage {
 	public static void main(String[] args) {
 		
 		System.out.print("hello");
-		
+		System.out.print("Hello");
+		System.out.print("Hi");
+		int a=10;
+	
 	}
+	
 
 }
